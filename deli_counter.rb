abf644fcc4fd8fe57fe1abfb69954ea39ada7842
Audrea Cook <audreacwork@gmail.com>
2019-07-15 19:50:35 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,6 +2,10 @@ def line(arr)
   if arr.size == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently"
+    list = []
+    arr.each_with_index do |name, index|
+      list << "#{index + 1}. #{name}"
+    end
+    puts "The line is currently #{list}"
   end
 end
